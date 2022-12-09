4dfef1db23f5cd8b750549a348565e65d729dffa
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2022-12-09 11:42:59 +0530

    added more cards and beatify the cards

--- src/components/home/mainContent/homes/Home.jsx
+++ src/components/home/mainContent/homes/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import "./style.css"
+import Popular from "../popular/Popular"
 
 const Home = () => {
   return (
     <>
       <main>
         <div className='container'>
-          <section className='mainContent'></section>
+          <section className='mainContent'><Popular/></section>
           <section className='sideContent'></section>
         </div>
       </main>
