2da9c19b8881b28b15433492f52d8104e8e8d94d
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2022-12-11 10:34:45 +0530

    added more boxes of info

--- src/components/home/mainContent/homes/Home.jsx
+++ src/components/home/mainContent/homes/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import "./style.css"
 import Popular from "../popular/Popular"
+import Ppost from "../Ppost/Ppost"
 
 const Home = () => {
   return (
     <>
       <main>
         <div className='container'>
-          <section className='mainContent'><Popular/></section>
+          <section className='mainContent'>
+            <Popular/>
+            <Ppost/>
+            </section>
           <section className='sideContent'></section>
         </div>
       </main>
