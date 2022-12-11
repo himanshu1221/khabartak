4d47a2d3f58c8015adef30cb650b1b4729a58bd3
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2022-12-11 12:18:32 +0530

    added social media handle and newsletter button

--- src/components/home/SideContent/Side/Side.jsx
+++ src/components/home/SideContent/Side/Side.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Heading from '../../../Heading/Heading'
 import SocialMedia from '../Social/SocialMedia'
-
+import './side.css'
 
 const Side = () => {
   return (
     <>
         <Heading title = "stay conneted"/>
         <SocialMedia/>
+        <Heading title = "Subscribe"/>
+          <section className='subscribe'>
+          <h1 className='title'>Subscribe to our NewsLetter</h1>
+          <form action="">
+            <input type="email" placeholder='Email Address..' />
+            <button>
+              <i className='fa fa-paper-plane'></i> Submit
+            </button>
+          </form>
+          </section>
     </>
   )
 }
--- src/components/home/SideContent/Social/SocialMedia.jsx
+++ src/components/home/SideContent/Social/SocialMedia.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Side/side.css'
 
 const SocialMedia = () => {
   return (
@@ -6,11 +7,23 @@ const SocialMedia = () => {
         <section className="social">
             <div className="socBox">
                 <i className='fab fa-facebook-f'></i>
-                <span>40,000 Likes</span>
+                <span>40,765 Likes</span>
             </div>
             <div className="socBox">
                 <i className='fab fa-pinterest'></i>
-                <span>40,000 Likes</span>
+                <span>4567 Fans</span>
+            </div>
+            <div className="socBox">
+                <i className='fab fa-twitter'></i>
+                <span>500 Followers</span>
+            </div>
+            <div className="socBox">
+                <i className='fab fa-instagram'></i>
+                <span>22,345 Followers</span>
+            </div>
+            <div className="socBox">
+                <i className='fab fa-youtube'></i>
+                <span>12,542 Subscriber</span>
             </div>
         </section>
     </>
